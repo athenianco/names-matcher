9b3319e4edb04c115688f72bb2d5bfabf0265165
Vadim Markovtsev <vadim@athenian.co>
2022-06-13 21:37:40 +0200

    [DEV-4306] Bump lapjv
    Signed-off-by: Vadim Markovtsev <vadim@athenian.co>

--- names_matcher/tests/test_main.py
+++ names_matcher/tests/test_main.py
@@ -1,3 +1,5 @@
+import os
+import platform
 import sys
 from tempfile import NamedTemporaryFile
 from unittest.mock import patch
@@ -6,17 +8,28 @@ from names_matcher.__main__ import main
 
 
 def test_main(capsys):
-    with NamedTemporaryFile(mode="w+", prefix="test_names_matcher_") as f1:
+    is_windows = platform.system() == "Windows"
+    with NamedTemporaryFile(mode="w+", prefix="test_names_matcher_",
+                            delete=not is_windows) as f1:
         f1.write("""Vadim Markovtsev
 Waren Long""")
         f1.flush()
-        with NamedTemporaryFile(mode="w+", prefix="test_names_matcher_") as f2:
+        with NamedTemporaryFile(mode="w+", prefix="test_names_matcher_",
+                                delete=not is_windows) as f2:
             f2.write("""Long Waren
 vmarkovtsev|vadim
 """)
             f2.flush()
+            if is_windows:
+                f1.close()
+                f2.close()
             with patch.object(sys, "argv", ["", f1.name, f2.name]):
-                main()
+                try:
+                    main()
+                finally:
+                    if is_windows:
+                        os.remove(f1.name)
+                        os.remove(f2.name)
             assert capsys.readouterr().out == """vmarkovtsev|vadim
 Long Waren
 """
